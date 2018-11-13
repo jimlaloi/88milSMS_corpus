294da3864c5d1efd01284ee23bdf404ad88cf60c
Jim Law <34045469+jimlaloi@users.noreply.github.com>
2018-11-13 14:41:14 -0600

    Update FrenchSMSCorpusScript.py

--- FrenchSMSCorpusScript.py
+++ FrenchSMSCorpusScript.py
@@ -1,8 +1,14 @@
-f = open('FrenchSMScorpus.txt', 'rU')
+# Download the 88milSMS corpus as .ods from http://88milsms.huma-num.fr/corpus.html (You need to enter credentials).
+# You want the corpus in a .txt file with the following format: The content of each SMS followed by <SMS>, in order to separate each SMS.
+# You can do this by adding a column in Excel to the right of the SMS_ANON column, and filling that column with the text <SMS> in every cell.
+# Then copy those two columns to a .txt file, name it FrenchSMScorpus.txt, and put it in your working directory.
+
+f = open('FrenchSMScorpus.txt', 'rU') # Open the .txt file
 text = f.read()
-SMSlist = text.split('<SMS>')
+SMSlist = text.split('<SMS>') # Separate the corpus so each SMS is an item in a list. Make sure you've followed the instructions in the comments above.
 f.close()
 
+# This script allows you to enter a search term, and it prints the number of SMS that contain that term as well as the full text of the first 5 SMS that contain it.
 searchterm = 'searchterm'
 i = 5
 while searchterm != 'exit':
